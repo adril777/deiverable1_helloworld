b3423f43b498d079c13ecbc0612b3630920d92b8
adril777 <122954464+adril777@users.noreply.github.com>
2023-01-17 23:28:58 -0500

    Add files via upload

---
+++ deliverable hello world/deliverable hello world/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace deliverable_helloworld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string stg = "Hello, World! (2)";
+            Console.WriteLine(stg);
+        }
+    }
+}
